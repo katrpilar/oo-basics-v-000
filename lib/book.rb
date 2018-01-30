b3943377bcda69783e992d8847898da8ecdd4bf5
Katrina Brinson <kbrinso@gmail.com>
2018-01-30 00:28:55 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,40 +1,16 @@
 class Book
+  attr_accessor :author, :page_count, :genre
+ 
   def initialize(title)
     @title = title
   end
-  
-  attr_accessor(:title)
-  
-  def title 
+ 
+  def title
     @title
   end
-  
-  def author=(author)
-    @author = author
-  end
-  
-  def author
-    @author
-  end
-  
-  def page_count=(num)
-    @page_count = num
-  end
  
-  def page_count
-    @page_count
-  end
-  
-  def genre=(genre)
-    @genre = genre
-  end
- 
-  def genre
-    @genre
-  end
-  
-  def 
+  def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
+ 
 end
-
